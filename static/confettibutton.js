b0fd33dca2206ae1705b6554f0c6ff708bc88614
imm <143816704+imm-a-bot@users.noreply.github.com>
2024-06-16 09:06:14 -0500

    Update and rename confetti.js to confettibutton.js

--- static/confettibutton.js
+++ static/confettibutton.js
@@ -3,7 +3,31 @@ const button = document.getElementById('confettiButton');
 button.addEventListener('click', function() {
     nConfetti = window.innerWidth / 2
     createNConfetti(nConfetti);
+
+    changeHeaderText();
 });
+
+const messages = [
+    "Happy Birthday Nysis !!!!",
+    "12/10 BIRTHDAY FOR A 12/10 GUY",
+    "miio was here n wishes you a happy birfday ! :3",
+    "greatest day ever for the coolest guy ever",
+    "hope this is the best day ever!",
+    "BIRF DAY YAY",
+    "WOOOOOOO CONFETTI HAPPY BIRTHDAY"
+];
+
+
+function getRandomMessage() {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    return messages[randomIndex];
+}
+
+
+function changeHeaderText() {
+    const header = document.getElementById('message');
+    header.textContent = getRandomMessage();
+}
 
 function createNConfetti(n) { 
     const fragment = document.createDocumentFragment();
